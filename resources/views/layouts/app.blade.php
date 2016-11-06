4507a5c89a71d22d8ba0538bffec653c4f0de05d
RajicDenis <denis.rajic92@gmail.com>
2016-11-06 18:46:07 +0100

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,368 +89,67 @@
 	});	
 
 	</script>
-
+	
 	<style>
-	html {
-		height: 100%;
-	}
-	body {
-		min-height: 100%;
-		display: flex;
-		flex-direction: column;
-		background-image: url({{ URL::asset('images/main-bg2.jpg')  }});
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-attachment: fixed;
-		font-family: 'Acme', sans-serif;
-	}
-	a {
-		color: white !important;
-		text-decoration: none !important;
-	}
-	a:hover {
-		transform: scale(1.1);
-	}
-	.ul {
-		padding: 0;
-		margin: 0;
-		height: 150px;
-	}
-	.l {
-		list-style: none;
-		padding-top: 15px;
-	}
-	.l:before {    
-	    font-family: 'FontAwesome';
-	    content: '\f06c';
-	    color: #33AFCB;
-	    margin:0 5px 0 -15px;
-	}
-	.l:hover {
-		transform: scale(1.07);
-	}
-	h4 {
-		color: white;
-	}
-	.login-txt {
-		color: white;
-		float: right;
-		padding-left: 10px;
-		padding-right: 10px;
-		border-left: 1px solid white;
-	}
-	.content {
-		position: relative;
-		display: flex;
-		flex-direction: row;
-		padding: 15px;
-		background: #F7F7F7;
-		flex: 1;
-		border: 3px solid #ddd;
-		border-radius: 20px;
-		box-shadow: 0px 0px 15px 2px black;
-		z-index: 2;
-		margin-top: 10px;
-		margin-bottom: 1px;
-	}
-	.top-header {
-		height: 30px;
-		background: #03467B;
-		border-bottom: 1px solid white;
-	}
-	.banner {
-		height: 140px;
-		background: transparent;
-	}
-	.logo {
-		margin-top: 20px;
-		height: 80px;
-		width: 90px;
-	}
-	.logo-box {
-		display: inline-block;
-		padding-left: 30px;
-	}
-	.logo-title {
-		display: inline-block;
-		margin-top: 20px;
-		padding-left: 20px;
-		color: white;
-		height: 80px;
-		vertical-align: middle;
-		font-size: 40px;
-		font-weight: 900;
-		line-height: 80px;
-		font-style: italic;
-		font-family: 'Lobster', sans-serif;
-	}
-	.search_box {
-		display: inline-block;
-		margin-top: 20px;
-		width: 300px;
-		height: 80px;
-		float: right;
-		line-height: 80px;
-		padding-right: 30px;
-	}
-	.form-control {
-		float: none !important;
-	}
-	.navbar {
-		background: #03467B;
-		background-repeat: repeat;
-		background-size: cover;
-		border-bottom: 1px solid white;
-		margin-bottom: 0 !important;
-		border-radius: 10px;
-	}
-	.footer {
-		display: flex;
-		align-items: center;
-		height: 300px;
-		background: #03467B;
-		border-top: 1px solid white;
-	}
-	.footer-content {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-	}
-	.copyright {
-		color: white;
-		text-align: right;
-		padding-top: 30px;
-	}
-	.fa-home {
-		color: white;
-	}
-	.nav-link {
-		padding-top: 5px;
-		border-right: 1px solid white;
-		margin-right: 5px;
-		font-family: 'Amaranth', sans-serif !important;
-		font-size: 16px;
-	}
-	.nav-link:hover {
-		background: #03467B; 
-		transform: scale(1.1);
-		z-index: 3;
-	}
-	.social {
-		position: relative;
-		width: 200px;
-		height: 220px;
-		position: fixed;
-		margin-top: 30px;
-		perspective: 1000px;
-		list-style: none;
-		top: 40%;
-		padding-left: 0;
-		z-index: 1;	
-	}
-	.social li a {
-		display: inline-block;
-		height: 40px;
-		width: 40px;
-		background: #222;
-		border-bottom: 1px solid #333;
-		font: normal normal normal
-		16px/20px 
-		'FontAwesome', 'Source Sans Pro', Helvetica, Arial, sans-serif;
-		color: #fff;
-		-webkit-font-smoothing: antialiased;
-		padding: 10px;
-		text-decoration: none;
-		text-align: center;
-		transition: background .5s ease .300ms
-	}
-
-	.social li:first-child a:hover { background: #3b5998 }
-	.social li:nth-child(2) a:hover { background: #00acee }
-	.social li:nth-child(3) a:hover { background: #ea4c89 }
-	.social li:nth-child(4) a:hover { background: #dd4b39 }
-
-	.social li:first-child a { border-radius: 0 5px 0 0 }
-	.social li:last-child a { border-radius: 0 0 5px 0 }
-	    
-	.social li a span {
-		width: 100px;
-		float: left;
-		text-align: center;
-		background: #222;
-		color: #fff;
-		margin: -25px 74px;
-		padding: 8px;
-		transform-origin: 0;
-		visibility: height: 	;n;
-		opacity: 0;
-		transform: rotateY(45deg);
-		border-radius: 5px;
-		transition: all .5s ease .300ms;
-	}
-
-	.social li span:after {
-		content: '';
-		display: block;
-		width: 0;
-		height: 0;
-		position: absolute;
-		left: -20px;
-		top: 7px;
-		border-left: 10px solid transparent;
-		border-right: 10px solid #222;
-		border-bottom: 10px solid transparent;
-		border-top: 10px solid transparent;
-	}
-
-	.social li a:hover span {
-		visibility: visible;
-		opacity: 1;
-		transform: rotateY(0);
-	}
-	/*Side content CSS */
-	.side-content {
-		border: 1px solid #ddd;
-		border-radius: 10px;
-		margin-right: 5px;
-		padding: 0;
-		background: white;
-	}
-	.panel {
-		border: none;
-	}
-	.panel-body {
-		padding: 0 0 15px 0;
-	}
-	.panel-heading {
-		border-bottom: none;
-		background-color: #03467B !important;
-		border-radius: 10px;
-	}
-	.panel-title {
-		color: white;
-		font-family: 'Acme', sans-serif;
-		text-align: center;
-		cursor: pointer;
-		display: block;
-	}
-	.list-group-item {
-		border-left: none;
-		border-right: none;
-		font-family: 'Acme', sans-serif;
-		text-align: center !important;
-	}
-	.side-info {
-		padding: 10px;
-		padding-bottom: 40px;
-		border-bottom: 1px solid #f5f5f5;
-	}
-	.supp-info {
-		color: #A6A6A6;
-	}
-	.support {
-		display: flex;
-		flex-direction: row;
-	}
-	.inf-icon {
-		display: block;
-		height: 100%;
-		width: 100px;
-		float: left;
-		margin: auto auto;
-		text-align: center;
-		color: #2F8DCD;
-	}
-	.tel {
-		margin-top: 20px;
-		color: #A6A6A6;
-	}
-	.flag {
-		width: 23px;
-		height: 17px;
-		margin-right: 15px;
-		margin-left: 5px;
-	} /*END side-content */
-	.push {
-		top: 280px;
-	}
-	@media screen and (max-width: 800px) {
+		html {
+			height: 100%;
+		}
 		body {
+			min-height: 100%;
+			display: flex;
+			flex-direction: column;
+			background-image: url({{ URL::asset('images/main-bg2.jpg')  }});
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+			font-family: 'Acme', sans-serif;
+		}
+		a {
+			color: white !important;
+			text-decoration: none !important;
+		}
+		a:hover {
+			transform: scale(1.1);
+		}
+		.star-rating {
+			position: relative;
+			display: inline-block;
+			background-image: url({{ URL::asset('images/star.png') }});
+			background-repeat: repeat-x;
+			width: 105px;
+			height: 25px;
+		}
+		.star-ratingInner {
+			position: absolute;
+			background-image: url({{ URL::asset('images/star.png') }});
+			background-position: 0 -30px; 
+			width: 105px;
+			height: 20px;
+			top: 0px;
+			left: 0;
+		}
+		.r5 {
+			width: 105px;
+		}
+		.r4 {
+			width: 84px;
+		}
+		.r3 {
+			width: 62px;
+		}
+		.r2 {
+			width: 41px;
+		}
+		.r1 {
+			width: 20px;
+		}
 
-			background-image: none;
-			background: #0073AA; 
-		}
-		.navbar {
-		    height: 40px;
-		    position: absolute;
-		    background: #0073AA;
-		    border: none;
-		    top: 50px;
-		    right: 10px;
-		    
-		}
-		.navbar-collapse {
-			border-color: #0073AA;
-		    border: none;
-		    box-shadow: none;
-		    overflow: hidden;
-		}
-		.nav-link {
-			border: none;
-		}
-		.nav-link:hover {
-			background: none;
-			transform: scale(1);
-		}
-		.logo-box {
-			padding-left: 0;
-		}
-		.search_box {
-			display: none;
-		}
-		.content {
-			transition-property: top, bottom;
-        	transition-duration: 0.5s;
-        	flex-direction: column;
-		}
-		.side-content {
-			width: 100% !important;
-		}
-		.side-info {
-			display: none;
-		}
-		.panel {
-			margin-bottom: 0;
-		}
-		.panel-title {
-			cursor: pointer;
-		}
-		.panel-body {
-			display: none;
-		}
-		.footer {
-			height: 500px;
-		}
-		.footer-content {
-			flex-wrap: wrap;		
-		}
-		.ftr {
-			width: 90%;
-		}
-	}
+	</style>
 
-	@media screen and (max-width: 400px) { 
-		.navbar {
-			top: 130px;
-			right: 120px;
-		}
-		.content {
-			margin-top: 50px;
-		}
-		.logo-box {
-			padding-left: 50px;
-		}
-	}
+	<link rel="stylesheet" media="all" type="text/css" href="{{ URL::asset('css/layout.css') }}">
+	<link rel="stylesheet" media="all" type="text/css" href="{{ URL::asset('css/sidebar-hover.css') }}">
+	<link rel="stylesheet" media="all" type="text/css" href="{{ URL::asset('css/homepage.css') }}">
+
 	</style>
 
 </head>
@@ -613,123 +312,6 @@
 									<div class="psu c-box" style="background-image: url({{ URL::asset('images/icons/psu-bg.jpg') }});"><div class="overlay"></div><span class="c-name">PSU</span></div>
 								</div>					    	
 					    	</div>
-							
-							<style>
-								.cloud {
-									position: absolute;
-									display: block;
-									width: 300px;
-									height: 215px;
-									left: 190px;
-									margin-top: -80px;
-								    border: 1px solid black;
-								    border-radius: 10px;
-								    z-index: 4;
-								    background: white;
-								    box-shadow: 0px 0px 10px 3px rgba(0,0,0,0.5);;
-								    transform: scale(0);
-								    transform-origin: left;
-								    transition: all 0.3s cubic-bezier(0.42, 0, 0.77, 1.44) 0s;
-								}
-								/* creates the larger triangle */
-								.cloud:before {
-								  content: ' ';
-								  position: absolute;
-								  width: 0;
-								  top: 84px;
-								  bottom: auto;
-								  left: -30px;								  
-								  border-style: solid;
-								  border-width: 16px 30px 16px 0;
-    							  border-color: transparent rgba(0,0,0,0.4);
-    							  display: block;
-								}
-								.c1:before {
-									top: 55px;
-								}
-								.c1:after {
-									top: 61px !important;
-								}
-								/* creates the smaller  triangle */
-								.cloud:after {
-								  content: ' ';
-								  position: absolute;
-								  width: 0;
-								  left: -21px;
-								  top: 90px;
-								  bottom: auto;
-								  border-width: 10px 21px 10px 00;
-    							  border-color: transparent #fff;
-    							  display: block;
-    							  border-style: solid;
-								}
-								.c1 {
-									margin-top: -50px;
-									text-align: center;
-								    padding-top: 40px;
-								    height: 150px;
-								}
-								.c9 {
-									padding-left: 25px;
-									padding-top: 25px;
-								}
-								.c-box {
-									display: inline-block;
-									width: 70px;
-									height: 70px;
-									margin-right: 15px;
-									margin-bottom: 15px;
-									box-shadow: 0 0 7px 3px rgba(0,0,0,0.4);
-									border-radius: 50%;
-									background-size: cover;
-									background-repeat: no-repeat;
-									background-position: center;
-									cursor: pointer;
-								}
-								.c-box:hover > .overlay {
-								    width:70px;
-								    height:70px;
-								    position:absolute;
-								    background-color:#000;
-								    opacity:0.5;
-								    border-radius: 50%;
-								    -webkit-animation: pulse 1s linear infinite;
-									animation: pulse 1s linear infinite;
-									box-shadow: 0 0 0 3px rgba(0,0,0,0.7);
-								}
-
-								.f {
-									position: relative;
-									display: flex;
-									flex-direction: row;
-								}
-								.c-name {
-									display: none;
-									position: absolute;
-									line-height: 70px;
-									width: 70px;									
-									color: white;
-									text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-									text-align: center;
-									opacity: 1 !important;
-								}
-								@-webkit-keyframes pulse {
-								    0% { -webkit-transform: scale(1); }
-								    50% { -webkit-transform: scale(1.1); }
-								    100% { -webkit-transform: scale(1); }
-								}
-								@keyframes pulse {
-								    0% { transform: scale(1); }
-								    50% { transform: scale(1.11); }
-								    100% { transform: scale(1); }
-								}
-								.pulse {
-								    -webkit-animation-name: pulse;
-								    animation-name: pulse;
-								}
-								
-
-							</style>
 
 						</div>
 
@@ -779,7 +361,7 @@
 					<h4>Abouts Us</h4>
 					<ul class="ul">
 						<li class="l"><a href="#">History</a></li>
-						<li class="l"><a href="#">Company Mission $ Vision</a></li>
+						<li class="l"><a href="#">Company Mission & Vision</a></li>
 						<li class="l"><a href="#">Contact Us</a></li>
 						<li class="l"><a href="#">FAQ</a></li>
 
